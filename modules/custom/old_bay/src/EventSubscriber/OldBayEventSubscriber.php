d4cee36e545dd46c57099d05427aefe45b9082c7
Ryan Weaver <ryan@thatsquality.com>
2017-04-23 22:22:40 -0400

    Step 13: Event: Fill in onKernelRequest

--- modules/custom/old_bay/src/EventSubscriber/OldBayEventSubscriber.php
+++ modules/custom/old_bay/src/EventSubscriber/OldBayEventSubscriber.php
@@ -2,6 +2,16 @@
 
 namespace Drupal\old_bay\EventSubscriber;
 
-class OldBayEventSubscriber {
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+class OldBayEventSubscriber implements EventSubscriberInterface {
+  public function onKernelRequest() {
+    drupal_set_message('Grab the old bay! We are cooking!');
+  }
+
+  public static function getSubscribedEvents() {
+    return [
+      'kernel.request' => 'onKernelRequest'
+    ];
+  }
 }
