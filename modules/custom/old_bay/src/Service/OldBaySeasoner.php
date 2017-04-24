659707ee2807b5f7abdb26a0505ba5d7abf71eac
Ryan Weaver <ryan@thatsquality.com>
2017-04-23 22:22:36 -0400

    Step 10: DI: Use DI for ConfigFactory

--- modules/custom/old_bay/src/Service/OldBaySeasoner.php
+++ modules/custom/old_bay/src/Service/OldBaySeasoner.php
@@ -2,10 +2,19 @@
 
 namespace Drupal\old_bay\Service;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+
 class OldBaySeasoner {
+
+  private $configFactory;
+
+  public function __construct(ConfigFactoryInterface $configFactory) {
+    $this->configFactory = $configFactory;
+  }
+
   public function seasonSomeRandomFood($count) {
     if ($count === null) {
-      $count = $this->config('old_bay.default')
+      $count = $this->configFactory->get('old_bay.default')
         ->get('default_count');
     }
 
